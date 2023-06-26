67b5950df55ad03d439b687657b8ce6026163c81
Gonzalo1q2w <gl6970680@gmail.com>
2023-06-25 23:23:36 -0500

    primer commit

---
+++ pepito/main.js
@@ -0,0 +1,12 @@
+
+const audioPlayer = document.getElementById('audio-player');
+const playBtn = document.getElementById('play-btn');
+const pauseBtn = document.getElementById('pause-btn');
+
+playBtn.addEventListener('click', () => {
+  audioPlayer.play();
+});
+
+pauseBtn.addEventListener('click', () => {
+  audioPlayer.pause();
+});
